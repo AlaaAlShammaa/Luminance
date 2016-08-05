15b25329a775b10790c0d988f9bb3a305ad3cd88
ALAA SHAMMAA <ALAA SHAMMAA>
2016-08-05 20:01:26 +0300

    Initial commit

---
+++ src/sample/BrightnessManager.java
@@ -0,0 +1,71 @@
+package sample;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+/**
+ * Created by AlaaShammaa on 8/2/2016.
+ */
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class BrightnessManager {
+    public static void setBrightness(int brightness)
+            throws IOException {
+        //Creates a powerShell command that will set the brightness to the requested value (0-100), after the requested delay (in milliseconds) has passed.
+        String s = String.format("$brightness = %d;", brightness)
+                + "$delay = 0;"
+                + "$myMonitor = Get-WmiObject -Namespace root\\wmi -Class WmiMonitorBrightnessMethods;"
+                + "$myMonitor.wmisetbrightness($delay, $brightness)";
+        String command = "powershell.exe  " + s;
+        // Executing the command
+        Process powerShellProcess = Runtime.getRuntime().exec(command);
+
+        powerShellProcess.getOutputStream().close();
+
+        //Report any error messages
+        String line;
+
+        BufferedReader stderr = new BufferedReader(new InputStreamReader(
+                powerShellProcess.getErrorStream()));
+        line = stderr.readLine();
+        if (line != null) {
+            System.err.println("Standard Error:");
+            do {
+                System.err.println(line);
+            } while ((line = stderr.readLine()) != null);
+
+        }
+        stderr.close();
+
+    }
+
+    public static int computeBrightness() throws AWTException {
+        Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+        BufferedImage bi = new Robot().createScreenCapture(screenRect);
+        int countLoop = 0;
+        double red = 0, green = 0, blue = 0;
+        for (int x = 0; x < bi.getWidth(); x++) {
+            for (int y = 0; y < bi.getHeight(); y++) {
+                Color c = new Color(bi.getRGB(x, y));
+                red += c.getRed();
+                green += c.getGreen();
+                blue += c.getBlue();
+                countLoop++;
+            }
+        }
+        red /= countLoop;
+        green /= countLoop;
+        blue /= countLoop;
+        return (int) Math.sqrt((red * red * .241) + (green * green * .691) + (blue * blue * .068));
+    }
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,165 @@
+package sample;
+import java.awt.*;
+import java.io.IOException;
+import com.sun.jna.Native;
+import com.sun.jna.platform.win32.WinDef.HWND;
+import com.sun.jna.ptr.PointerByReference;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import static sample.Main.User32DLL.GetForegroundWindow;
+import static sample.Main.User32DLL.GetWindowTextW;
+public class Main extends Application {
+    private Stage stage;
+    MainThread mainThread;
+    public static boolean running = true;
+    public static boolean noExit = true;
+    public static final String imageLoc = "http://i65.tinypic.com/161jb7l.png";
+    private static final int MAX_TITLE_LENGTH = 1024;
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.stage = primaryStage;
+        Platform.setImplicitExit(false);
+        javax.swing.SwingUtilities.invokeLater(this::addAppToTray);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        StackPane layout = new StackPane();
+        layout.setPrefSize(1, 1);
+        Scene scene = new Scene(layout);
+        scene.setFill(Color.TRANSPARENT);
+        stage.setScene(scene);
+        mainThread = new MainThread("MainThread");
+        mainThread.start();
+    }
+
+    private void addAppToTray() {
+        try {
+            java.awt.Toolkit.getDefaultToolkit();
+            if (!java.awt.SystemTray.isSupported()) {
+                System.out.println("No system tray support, application exiting. ");
+                Platform.exit();
+            }
+            java.awt.SystemTray tray = java.awt.SystemTray.getSystemTray();
+            URL imageurl = new URL(imageLoc);
+            java.awt.Image image = ImageIO.read(imageurl);
+            java.awt.TrayIcon trayIcon = new java.awt.TrayIcon(image);
+            java.awt.MenuItem stopItem = new java.awt.MenuItem("Stop");
+            // stop the execution of the thread
+            stopItem.addActionListener(event ->{
+                if (stopItem.getLabel().equals("Stop")) {
+                    stopItem.setLabel("Resume");
+                    running = false;
+                } else {
+                    stopItem.setLabel("Stop");
+                    running = true;
+                }
+            } );
+            java.awt.Font defaultFont = java.awt.Font.decode(null);
+            java.awt.Font boldFont = defaultFont.deriveFont(java.awt.Font.BOLD);
+            stopItem.setFont(boldFont);
+            java.awt.MenuItem exitItem = new java.awt.MenuItem("Exit");
+            exitItem.addActionListener(event -> {
+                Platform.exit();
+                tray.remove(trayIcon);
+                noExit = false;
+            });
+            final java.awt.PopupMenu popup = new java.awt.PopupMenu();
+            popup.add(stopItem);
+            popup.addSeparator();
+            popup.add(exitItem);
+            trayIcon.setPopupMenu(popup);
+            tray.add(trayIcon);
+        } catch (IOException | AWTException e) {
+            System.out.println("Unable to init system tray");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    private static class MainThread extends Thread {
+        Thread t;
+        String threadName;
+        String ForeWindTitle;
+        boolean changed = false;
+        char[] buffer = new char[MAX_TITLE_LENGTH * 2];
+
+        MainThread(String threadName) {
+            this.threadName = threadName;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            System.out.println("Running Thread is " + threadName);
+            GetWindowTextW(GetForegroundWindow(), buffer, MAX_TITLE_LENGTH);
+            ForeWindTitle = Native.toString(buffer);
+
+            while (noExit) {
+                while (!running)
+                    yield();
+                try {
+                    GetWindowTextW(GetForegroundWindow(), buffer, MAX_TITLE_LENGTH);
+                    if (!ForeWindTitle.equals(Native.toString(buffer))) {
+                        changed = true;
+                        ForeWindTitle = Native.toString(buffer);
+                    }else {
+                        changed = false;
+                    }
+                    if(changed) {
+                        int brightnessVal = BrightnessManager.computeBrightness();
+                        if (brightnessVal < 100 && brightnessVal > 80) {
+                            BrightnessManager.setBrightness(60);
+                        } else if (brightnessVal < 80 && brightnessVal > 60) {
+                            BrightnessManager.setBrightness(80);
+                        } else if (brightnessVal < 60) {
+                            BrightnessManager.setBrightness(100);
+                        } else if (brightnessVal > 100 && brightnessVal < 150) {
+                            BrightnessManager.setBrightness(40);
+                        } else if (brightnessVal > 150 && brightnessVal < 200) {
+                            BrightnessManager.setBrightness(20);
+                        } else {
+                            BrightnessManager.setBrightness(0);
+                        }
+                    }
+                    Thread.sleep(60);
+                } catch (AWTException | IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        @Override
+        public synchronized void start() {
+            super.start();
+            System.out.println("Starting Thread : " + threadName);
+            if (t == null) {
+                t = new Thread(this, threadName);
+                t.start();
+            }
+        }
+
+
+    }
+    static class User32DLL {
+        static { Native.register("user32"); }
+        public static native int GetWindowThreadProcessId(HWND hWnd, PointerByReference pref);
+        public static native HWND GetForegroundWindow();
+        public static native int GetWindowTextW(HWND hWnd, char[] lpString, int nMaxCount);
+    }
+}
